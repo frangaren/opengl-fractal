09c475160a5f5513908ec610dfc705150ee5cc74
Francisco Garcia <frangaren@usal.es>
2017-09-04 22:31:39 +0200

    Add mandelbrot set fragment shader

--- src/fragment_shader.glsl
+++ src/fragment_shader.glsl
@@ -1,9 +1,41 @@
 #version 330 core
 
+#define MAX_N 5000
+
 in vec4 fragment_position;
 
 layout(location = 0) out vec4 out_color;
 
+int mandelbrot_iteration_count(vec2 c);
+float modulus(vec2 v);
+vec2 complex_product(vec2 a, vec2 b);
+
 void main() {
-  out_color = (fragment_position + vec4(1.0f, 1.0f, 0.0f, 1.0f))/2.0f;
+  int n = mandelbrot_iteration_count(fragment_position.xy * 3.0f);
+  float c;
+  if (n == MAX_N) {
+    c = 0.0f;
+  } else {
+    c = float(n)/15.0f;
+  }
+  out_color = vec4(c, 0.33f*c, 0.0f, 1.0f);
+}
+
+int mandelbrot_iteration_count(vec2 c) {
+  vec2 z = c;
+  int n = 0;
+  while (modulus(z) < 4.0f && n < MAX_N) {
+    z = complex_product(z, z) + c;
+    n++;
+  }
+  return n;
+}
+
+float modulus(vec2 v) {
+  vec2 w = v*v;
+  return w.x + w.y;
+}
+
+vec2 complex_product(vec2 a, vec2 b) {
+  return vec2(a.x*b.x - a.y*b.y, a.x*b.y + a.y*b.x);
 }
